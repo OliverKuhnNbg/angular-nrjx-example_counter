d0ed2f26d89804c370124f8532819318cd821402
Oliver <oliver.kuhn@ancud.de>
2023-03-16 15:11:25 +0100

    addition of pump comp

---
+++ angular-state-management/src/app/components/show-pump/show-pump.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-show-pump',
+  templateUrl: './show-pump.component.html',
+  styleUrls: ['./show-pump.component.scss']
+})
+export class ShowPumpComponent {
+
+}
