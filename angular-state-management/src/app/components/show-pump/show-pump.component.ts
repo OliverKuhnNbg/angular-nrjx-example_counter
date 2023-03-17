b3d213b535767d8f1ac544658c0e404fc6c77351
Oliver <oliver.kuhn@ancud.de>
2023-03-17 09:30:43 +0100

    init THREE JS Lib

--- angular-state-management/src/app/components/show-pump/show-pump.component.ts
+++ angular-state-management/src/app/components/show-pump/show-pump.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, Input, ViewChild } from '@angular/core';
+import * as THREE from "three";
+import { Scene } from 'three';
 
 @Component({
   selector: 'app-show-pump',
@@ -7,4 +9,34 @@ import { Component } from '@angular/core';
 })
 export class ShowPumpComponent {
 
+ ngOnInit() {
+    const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+    const renderer = new THREE.WebGLRenderer();
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    document.body.appendChild( renderer.domElement );
+
+    const geometry = new THREE.BoxGeometry( 2, 2, 2 );
+    const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+    const cube = new THREE.Mesh( geometry, material );
+
+    // add custom mesh
+    
+
+    scene.add( cube );
+
+    camera.position.z = 5;
+
+    function animate() {
+      requestAnimationFrame( animate );
+
+      cube.rotation.x += 0.01;
+      cube.rotation.y += 0.01;
+
+      renderer.render( scene, camera );
+    }
+
+    animate();
+ }
 }
